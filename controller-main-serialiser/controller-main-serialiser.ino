cd9aa6a172ff3a8fd4ba0923b8551bd42135cd61
Al Bennett <al@hotgluetech.com>
2015-04-17 20:28:15 +0100

    Added serialiser sketch

---
+++ controller-main-serialiser/controller-main-serialiser.ino
@@ -0,0 +1,27 @@
+/*
+ * Write a serial number to the first EEPROM address and print it out to the serial
+ */
+
+#include <EEPROM.h>
+
+// the current address in the EEPROM (i.e. which byte
+// we're going to write to next)
+
+const byte serial_no = 16;
+
+void setup()
+{
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+
+  EEPROM.write(0, serial_no);
+}
+
+void loop()
+{
+  Serial.print("Serial: ");
+  Serial.println(EEPROM.read(0));
+  while (1);
+}
