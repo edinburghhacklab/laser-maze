042a890251f1181a640864b4c5dbc821ff009cc8
Kit Barnes <kit@ninjalith.com>
2015-04-18 17:10:32 +0100

    Add rudiments of tornado-based game loop

---
+++ python/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(name='hacklab-lasers',
+      author='KitB',
+      author_email='kit@ninjalith.com',
+      packages=['lasers'],
+      zip_safe=False,
+      install_requires=[
+          'Logbook',
+          'click',
+          'pyserial',
+          'tornado',
+      ],
+      scripts=['bin/lasers'],
+      )
