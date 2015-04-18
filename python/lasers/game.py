531b8a75db1eed4405e549a8798523a83c79d8b3
Kit Barnes <kit@ninjalith.com>
2015-04-18 20:20:55 +0100

    Add laser break detection

--- python/lasers/game.py
+++ python/lasers/game.py
@@ -33,6 +33,20 @@ class Node(object):
 class GameState(object):
     def __init__(self):
         self.nodes = {}
+        self.laser_broken = lambda i: None  # default to noop, set from outside
+        self.n_lasers_broken = lambda: None  # "
+        self.n_broken_threshold = 5
+
+    def game_start(self):
+        logger.info('Game Start!')
+        self.all_on()
+        self.n_broken = 0
+
+    def all_on(self):
+        logger.info('Turning all the lasers')
+        self.talker.send('\0{}{}'.format(chr(255), chr(2)))
+        #for node in self.nodes:  # iterating through keys
+        #    self.talker.send('\0{}{}'.format(chr(node), chr(2)))
 
     @property
     def talker(self):
@@ -45,10 +59,32 @@ class GameState(object):
         talker.data_received = self._receive
         talker.include_self = False
 
+    def _laser_broken(self, index):
+        self.n_broken += 1
+        self.laser_broken(index)
+        if self.n_broken >= self.n_broken_threshold:
+            self._n_lasers_broken()
+
+    def _n_lasers_broken(self):
+        logger.warn('{} lasers broken'.format(self.n_broken_threshold))
+        self.n_lasers_broken()
+
     def _receive(self, line):
         index, state = parse_line(line)
-        if index is not None:
+        if index is None:
+            logger.debug(line.strip())
+        else:
             logger.debug('Node {} in state {}', index, state)
+            try:
+                old_state = self.nodes[index]
+            except KeyError:
+                pass
+            else:
+                if (old_state, state) == (NodeState.normal, NodeState.broken):
+                    # laser was just broken
+                    logger.info('Laser broken {}', index)
+                    self._laser_broken(index)
+
             self.nodes[index] = state
 
 
@@ -58,6 +94,10 @@ def run_game(serial_device, baudrate):
     gs = GameState()
     gs.talker = st
 
+    repeat_on = ioloop.PeriodicCallback(gs.all_on, 3000)
+    repeat_on.start()
+
+    ioloop.IOLoop.instance().call_later(2, gs.game_start)
     ioloop.IOLoop.instance().start()
 
 
