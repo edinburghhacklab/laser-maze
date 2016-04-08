0d453c4c67462603c973610c486ba34550f4dc5e
greenhac <peter@greenhac.org.uk>
2016-04-08 11:24:06 +0100

    Added files via upload
    Added modulation of Laser for working better goodness. 
    
    ENABLE pin is now LOW to set the LASER on, HIGH to switch laser off.

--- attiny85_laser_control/attiny85_laser_control.ino
+++ attiny85_laser_control/attiny85_laser_control.ino
@@ -12,7 +12,7 @@
 #define LASER_ENABLE_PIN      4 
 #define SENSOR_PIN            3              // ATtiny Pin 2
 #define LASER_BROKEN_OUT_PIN  2              // attiny pin 7
-#define LASER_TEST_PIN        1              // attiny pin 1
+#define LASER_TEST_PIN        1              // attiny pin 6
 #define LASER_OUTPUT          0 // IR sensor - ATtiny Pin 5 
 
 #define LASER_ON       1
@@ -21,76 +21,95 @@
 
 #define BEAM_BREAK_MIN    20 // ms
 
-volatile unsigned long lastRiseTime = 0;
-volatile unsigned long lastFallTime = 0;
-volatile int laserState = 0;
+//volatile unsigned long lastRiseTime = 0;
+volatile long lastFallTime = 0;
+/*volatile*/ int laserState = LASER_OFF;
+volatile long lastGoodTime = 0;
 
 void setup(){
-  pinMode(LASER_OUTPUT,OUTPUT);
-  pinMode(LASER_ENABLE_PIN, INPUT);
-  pinMode(LASER_TEST_PIN, INPUT);  
-  pinMode(SENSOR_PIN,INPUT);
-  pinMode(LASER_BROKEN_OUT_PIN, OUTPUT);           
-    // switch laser off
-  digitalWrite(LASER_OUTPUT, LOW);
   
+  pinMode(LASER_OUTPUT,OUTPUT);
+  pinMode(LASER_ENABLE_PIN, INPUT_PULLUP);
+  pinMode(LASER_TEST_PIN, INPUT_PULLUP);  
+  pinMode(SENSOR_PIN,INPUT_PULLUP);
+  pinMode(LASER_BROKEN_OUT_PIN, OUTPUT);           
+  // switch laser off
+  //digitalWrite(LASER_OUTPUT, LOW);
+  //TCCR0A |= (1<<COM0A1) | (1<<WGM00); // PWM Phase correct mode 1
+
+TCCR0A = 3<<COM0A0 | 3<<WGM00;
+TCCR0B = 0<<WGM02 | 3<<CS00;
+
+laserOff();
+// TCCR0B = 0<<WGM02 | 3<<CS00; // Optional; already set
+
+//  laserOn();
+  //analogWrite(LASER_OUTPUT,255*0.90);
   // Catch change of laser enable pin on attiny
-  attachPcInterrupt(SENSOR_PIN,laser_detect_isr,CHANGE);
+  
+  //delay(500);
+  //lastRiseTime= micros();
+
+//attachInterrupt(SENSOR_PIN, interrupt, RISING);
+  attachPcInterrupt(SENSOR_PIN,laser_detect_isr,FALLING);
+  //delay(100);
+  laserState = LASER_OFF;
+}
+
+void laserOn() {
+  OCR0A = 255 - (255*0.85);    
+}
+void laserOff() {
+  OCR0A = 255 ;    
 }
 
 void loop(){
   
-  unsigned long currentMillis = millis();  
-  
-  if (digitalRead(LASER_ENABLE_PIN) == HIGH && digitalRead(LASER_TEST_PIN) == HIGH) {
+  long currentMillis = millis();
+
+  if (currentMillis - lastGoodTime > 50 && laserState == LASER_ON) {
+    laserState = LASER_BROKEN;
+    laserOff();
+    //digitalWrite(LASER_OUTPUT, LOW);
+    digitalWrite(LASER_BROKEN_OUT_PIN, HIGH);
+    delay(100);
+    digitalWrite(LASER_BROKEN_OUT_PIN, LOW);
+    delay(10);
+    laserState = LASER_OFF;
+  }
+
+  if (digitalRead(LASER_ENABLE_PIN) == LOW && digitalRead(LASER_TEST_PIN) == LOW) {
+    // we are in test mode switch laser on
     laserState = LASER_ON;
-    digitalWrite(LASER_OUTPUT, HIGH);
-    lastFallTime = currentMillis;
+    lastGoodTime=millis();
+    laserOn();
+    //delay(10);
   }
   
-  if (digitalRead(LASER_ENABLE_PIN) == HIGH) { 
+  
+  if (digitalRead(LASER_ENABLE_PIN) == LOW) { 
     if (laserState == LASER_OFF) {
       // switch laser on
       laserState = LASER_ON;
-      lastFallTime = currentMillis;
-      digitalWrite(LASER_OUTPUT, HIGH);
-    }
-
-    if(digitalRead(SENSOR_PIN) == LOW && (currentMillis > lastFallTime + BEAM_BREAK_MIN)) {
-       // sensor pin, has been off for longer than BEAM_BREAK_MIN 
-       digitalWrite(LASER_OUTPUT, LOW);
-       digitalWrite(LASER_BROKEN_OUT_PIN, HIGH);
-       laserState = LASER_BROKEN;
-       delay(10);
-       digitalWrite(LASER_BROKEN_OUT_PIN, LOW);
-       laserState = LASER_OFF;
+      lastGoodTime=millis();
+      laserOn();
     }
   } else {
-    // arduino switched us off
-    digitalWrite(LASER_OUTPUT, LOW);
-    laserState = LASER_OFF;
-    delay(10);
+   // arduino switched us off
+   laserOff();
+   laserState = LASER_OFF;
   }
+  
+ 
 }
 
 void laser_detect_isr(){ 
   // This ISR is called for EACH change of the laser enable pin
-  int detectPinState = digitalRead(SENSOR_PIN);
-  unsigned long currentMillis = millis();
-  if (HIGH == detectPinState) {
-    // RISING
-    lastRiseTime = currentMillis;
-  } else if (LOW == detectPinState){
-    // FALLING
-    if(digitalRead(LASER_TEST_PIN) == LOW) {
-      if (currentMillis > lastFallTime + 20) { 
-        // last fall time > 20ms ago, we are in low, switch off LASER 
-        digitalWrite(LASER_OUTPUT, LOW);
-        laserState = LASER_BROKEN;
-      }
-    }
-    lastFallTime = currentMillis; 
+  long currentMillis = millis(); 
+  if ((currentMillis - lastFallTime > 10) && (currentMillis - lastFallTime < 20)) {
+    lastGoodTime = currentMillis;
   }
-}
+  lastFallTime = currentMillis;
 
+}
 
